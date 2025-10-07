d3d4feb6473e74c477b45b290ff04f5d896d02d0
Ethan Anderson <150651098+mite404@users.noreply.github.com>
2025-10-07 11:57:46 -0400

    Result of Andrew pair programming session

--- src/GameCanvas.tsx
+++ src/GameCanvas.tsx
@@ -1,16 +1,36 @@
 import { useEffect, useRef } from 'react';
-import { initGame } from './initGame';
-import { useGameStore, gameBoard } from './stores/gameStore';
+import { initApp, initGame, render } from './pixi/renderer'
+import { initialGameState } from './type';
+import type { Application } from 'pixi.js';
 
 export function GameCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const gameBoard = useGameStore((state) => state.gameBoard);
+  const appRef = useRef<Application>(null)
+  // const gameBoard = useGameStore((state) => state.gameBoard);
 
+  const gameState = initialGameState
+
+  async function setupPixi(canvas: HTMLCanvasElement) {
+    if (!appRef.current) {
+      const app = await initApp(canvas)
+      appRef.current = app
+    }
+  }
+
+  // SETUP the app
   useEffect(() => {
     if (canvasRef.current) {
-      initGame(canvasRef.current, gameBoard)
+      setupPixi(canvasRef.current)
     }
-  }, [gameBoard]);
+
+  }, [canvasRef])
+
+  // RE-RENDERS the app, whenever state changes
+  useEffect(() => {
+    if (canvasRef.current && appRef.current) {
+      render(appRef.current, gameState)
+    }
+  }, [gameState]);
 
   return <canvas ref={canvasRef} />
 }
--- src/type.ts
+++ src/type.ts
@@ -1,17 +1,33 @@
+
+export type GameState = {
+    player: Player;
+    phase: Phase;
+    grid: Grid;
+    wave: number;
+    enemies: Enemy[];
+    towers: Tower[];
+    pieces: PlacedPiece[]
+    longestPath: PathNode[] // this is the path that the enemies are going to take
+    validPath: boolean
+    winlose: 'Playing' | 'Win' | 'Lose'
+}
+
+
 export type Player = {
     currentHealth: number;
     maxHealth: number;
     armor?: number;
     gold: number;
-    allPieces: Piece[]; // all pieces in player library
-    levelPieces: Piece[]; // pieces given to player this wave
+    library: Piece[] // all pieces in the library
+    deck: Piece[]; // all pieces remaining in the deck
+    hand: Piece[]; // pieces in the player's hand
 }
 
-export type CellType= "empty" | "path" | 'tower' | 'blocked'
+export type CellType = "empty" | "path" | 'tower' | 'blocked'
 
 export type Cell = {
-    x:number;
-    y:number;
+    x: number;
+    y: number;
     type: CellType;
     occupiedBy?: string;
     terrain?: string //potential different effects later
@@ -21,17 +37,17 @@ export type Grid = Cell[][]
 
 export type Piece = {
     id: string;
-    shape: CellType[][];
+    shape: Piece[][];
     isPlaced: boolean;
-    rotation: 0 | 90| 180 | 270;
+    rotation: 0 | 90 | 180 | 270;
     flipped: boolean;
     tower: Tower;
 }
 
 export type PlacedPiece = {
     id: string;
-    InitialPieceId: string; // which initial piece was placed
-    position: {x: number; y: number} //top left corner of piece
+    pieceId: string; // which initial piece was placed
+    position: { x: number; y: number } //top left corner of piece
 }
 
 export type Enemy = {
@@ -39,10 +55,10 @@ export type Enemy = {
     type: string;
     currentHealth: number;
     maxHealth: number;
-    armor?:number;
-    speed: number; 
+    armor?: number;
+    speed: number;
     to: PathNode;
-    currentPosition: {x: number, y: number}
+    currentPosition: { x: number, y: number }
     gold: number;
 }
 
@@ -50,59 +66,46 @@ export type PathNode = {
     id: string;
     x: number;
     y: number
-} 
+}
 
 export type Tower = {
     id: string;
     type: string
     pieceId: string;
-    position: {x: number, y: number}
+    position: { x: number, y: number }
     damage: number
     armorDmg?: number
-    range:number
-    fireRate:number
-    cooldown:number
-    targetId?:string
-    buy?:number
-    sell?:number
+    range: number
+    fireRate: number
+    cooldown: number
+    targetId?: string
+    buy?: number
+    sell?: number
 }
 
 export type Phase = 'Build' | 'Defense' | 'End' | 'Shop'
 
-export type GameState = {
-    player:Player;
-    phase: Phase;
-    grid: Grid;
-    wave: number;
-    enemies: Enemy[];
-    towers: Tower[];
-    pieces: PlacedPiece[]
-    winlose: 'Playing' | 'Win' | 'Lose'
-    validPath: Boolean
-    path: PathNode[]
 
-}
-
-export const initialPlayer : Player = {
+export const initialPlayer: Player = {
     currentHealth: 100,
     maxHealth: 100,
     armor: 0,
     gold: 100,
-    allPieces: [], 
+    allPieces: [],
     levelPieces: []
 }
 
 function createEmptyGrid(): Grid {
-  return Array.from({ length: 10 }, (_, y) =>
-    Array.from({ length: 10 }, (_, x) => ({
-      x,
-      y,
-      type: 'empty' as CellType
-    }))
-  );
+    return Array.from({ length: 10 }, (_, y) =>
+        Array.from({ length: 10 }, (_, x) => ({
+            x,
+            y,
+            type: 'empty' as CellType
+        }))
+    );
 }
 
-export const initialGameState : GameState = {
+export const initialGameState: GameState = {
     player: initialPlayer,
     phase: 'Build',
     grid: createEmptyGrid(),

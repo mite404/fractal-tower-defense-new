3ad9d031d5b0aaaa4334b462e58e87524f32aa00
luebbertevan <luebbertevan@gmail.com>
2025-10-09 19:21:49 -0400

    only render inventory in build phase

--- src/pixi/renderer.tsx
+++ src/pixi/renderer.tsx
@@ -108,7 +108,7 @@ export async function initApp(canvas: HTMLCanvasElement): Promise<Application> {
 export function render(gameState: GameState) {
 	// render the board
 	renderBoard(gameState);
-	renderInventory(app, gameState);
+	if (gameState.phase === "Build") renderInventory(app, gameState);
 	// TODO render piece sidebar
 
 	// render enemies

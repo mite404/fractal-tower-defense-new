ebc02d90495606331d99e4c14b8837c4384052c0
Shouwang Huang <shouwangh82@gmail.com>
2025-10-10 18:58:19 -0400

    sprites for some reason

--- src/pixi/textures.ts
+++ src/pixi/textures.ts
@@ -3,9 +3,15 @@ import { Assets, Texture } from "pixi.js";
 // TODO: test if this actually works idk if this will work.
 export let bgTileTexture: Texture
 export let enemyDummyTexture: Texture
+export let tileTexture: Texture
+export let towerTexture: Texture
+export let pathTexture: Texture
 
 
 export async function loadTextures() {
   enemyDummyTexture = await Assets.load('ENEMY_dummy.png');
   bgTileTexture = await Assets.load('FIELD_tileset.png')
+  tileTexture = await Assets.load('./images/dune.png')
+  towerTexture = await Assets.load('./images/Tower_Blue.png')
+  pathTexture = await Assets.load('./images/path.png')
 }
